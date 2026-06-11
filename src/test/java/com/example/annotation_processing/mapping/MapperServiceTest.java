b6a6637ebd70cb32dc2f264578f90afc0cda7f13
slPerryRhodan <slxlogan@gmx.de>
2026-06-11 22:35:35 +0200

    Add issue description at the test

--- src/test/java/com/example/annotation_processing/mapping/MapperServiceTest.java
+++ src/test/java/com/example/annotation_processing/mapping/MapperServiceTest.java
@@ -9,7 +9,13 @@ class MapperServiceTest {
     @Test
     void map_mapstructMapper_implMapperShouldBeVisible() {
         // given
-        // FIXME: IntelliJ with WSL2 says "Cannot resolve symbol 'MyMapperImpl'", but the test is still working
+        // FIXME: IntelliJ with WSL2 cannot automatically resolve the class MyMapperImpl, which leads to
+        //  "Cannot resolve symbol 'MyMapperImpl'", but the test and the rest of the is still working.
+        // WORKAROUND for WSL2 which is not necessary on Windows system:
+            // 1: Mark generated code under main (build/generated/sources/annotationProcessor/java/main) as "Generated Source Root"
+            // 2: In this test class here execute at the unresolved MyMapperImpl class the context menu action "Add dependency on module 'annotation-processing'"
+            // 3: In MyMapperImpl class execute at @Component annotation the context menu action "Add library 'spring.context' to class path"
+            // 4: In MyMapperImpl class execute at MyMapper interface the context menu action "Add Add dependency on module 'annotation-processing.main'"
         var myMapperImpl = new MyMapperImpl();
         var mapperService = new MapperService(myMapperImpl);
         var source = new Source("property");
